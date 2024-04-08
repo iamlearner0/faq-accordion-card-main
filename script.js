7381e15c57655c0a4925a62c088c83da3855bdac
iamlearner0 <quadrisaheed001@gmail.com>
2024-04-08 23:16:08 +0100

    dropdown arrow

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 //variables
 const questions = document.querySelectorAll(".question-row");
+
 //event listeners
 
 questions.forEach((question) => {
   question.addEventListener("click", () => {
+    let arrow = question.lastElementChild;
     if (question.classList.contains("open")) {
       question.classList.remove("open");
+      arrow.classList.remove("rotated");
     } else {
       let isOthersOpen = document.querySelectorAll(".open");
       isOthersOpen.forEach((others) => {
         others.classList.remove("open");
       });
       question.classList.add("open");
+      arrow.classList.add("rotated");
     }
   });
 });
